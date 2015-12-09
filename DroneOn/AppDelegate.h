68849e005bb611de638650f31ee407165eed2720
Curt D Miller <curtdmiller@gmail.com>
2015-12-09 13:14:22 -0800

    Initial Commit

---
+++ DroneOn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DroneOn
+//
+//  Created by Curt Miller on 12/9/15.
+//  Copyright © 2015 Curt Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DroneOn/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  DroneOn
+//
+//  Created by Curt Miller on 12/9/15.
+//  Copyright © 2015 Curt Miller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
