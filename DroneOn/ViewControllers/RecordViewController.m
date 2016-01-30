aaeac08ae425b1da4efed400fd4b0a6514459a3f
Curt D Miller <curtdmiller@gmail.com>
2016-01-30 15:50:24 -0800

    updated interface to use changing color play button instead of on/off toggle

--- DroneOn/ViewControllers/RecordViewController.m
+++ DroneOn/ViewControllers/RecordViewController.m
@@ -14,6 +14,7 @@
 @property (weak, nonatomic) IBOutlet UIButton *recordButton;
 @property (weak, nonatomic) IBOutlet UIProgressView *recordProgress;
 @property (weak, nonatomic) IBOutlet UISlider *recordTuning;
+@property (weak, nonatomic) IBOutlet UIButton *recordPlayButton;
 
 @end
 
@@ -39,12 +40,21 @@ float rectime;
 - (void)receiveFloat:(float)received fromSource:(NSString *)source{
     if ([source isEqualToString:@"recTime"] == 1) {
         [self.recordProgress setProgress:received animated:NO];
+        if(received >= 1.0){
+            [self.recordPlayButton setTitleColor:[UIColor greenColor] forState:normal];
+        } else {
+            [self.recordPlayButton setTitleColor:[UIColor darkGrayColor] forState:normal];
+        }
     }
 }
 
 - (IBAction)recordTouched:(id)sender {
     [self.patch recordStartStop:1];
     [self.recordProgress setProgress:0 animated:NO];
+}
+- (IBAction)playButtonPressed:(id)sender {
+    
+    
 }
 - (IBAction)recToggleSwitchTouched:(id)sender {
     if (self.recToggleSwitch.isOn) {
